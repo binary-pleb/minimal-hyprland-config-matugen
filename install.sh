16d0bbe98b5bc212fa352a98173e5186bf0ce9d4
binary-pleb <imanactualfortnitekid@gmail.com>
2026-09-25 22:20:15 +1000

    added install script

---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env
+
+set -e
+
+echo "Installing required packages..."
+sudo pacman -S --needed grim slurp satty nemo ttf-jetbrains-mono-nerd awww firefox hyprland waybar rofi-wayland hyprlock
+
+CONFIG_DEST="$HOME/.config"
+
+echo "Installing dotfiles..."
+
+
+if [ -d "$CONFIG_DEST/hypr" ]; then
+    rm -rf "$CONFIG_DEST/hypr"
+fi
+mkdir -p "$CONFIG_DEST/hypr"
+cp "hypr"/* "$CONFIG_DEST/hypr"
+
+
+if [ -d "$CONFIG_DEST/kitty" ]; then
+    rm -rf "$CONFIG_DEST/kitty"
+    mkdir -p "$CONFIG_DEST/kitty"
+    cp "kitty"/* "$CONFIG_DEST/kitty"
+else
+    cp "kitty"/* "$CONFIG_DEST/kitty"
+fi
+
+
+if [ -d "$CONFIG_DEST/waybar" ]; then
+    rm -rf "$CONFIG_DEST/waybar"
+    mkdir -p "$CONFIG_DEST/waybar"
+    cp "waybar"/* "$CONFIG_DEST/waybar"
+else
+    cp "waybar"/* "$CONFIG_DEST/waybar"
+fi
+
+
+
+if [ -d "$CONFIG_DEST/rofi" ]; then
+    rm -rf "$CONFIG_DEST/rofi"
+    mkdir -p "$CONFIG_DEST/rofi"
+    cp "rofi"/* "$CONFIG_DEST/rofi"
+else
+    cp "rofi"/* "$CONFIG_DEST/rofi"
+fi
+
+
+
+if [ -d "$CONFIG_DEST/matugen" ]; then
+    rm -rf "$CONFIG_DEST/matugen"
+    mkdir -p "$CONFIG_DEST/matugen"
+    cp "matugen"/* "$CONFIG_DEST/matugen"
+else
+    cp "matugen"/* "$CONFIG_DEST/matugen"
+fi
+
+
+mkdir -p $HOME/.local/bin
+cp rofi-power-menu $HOME/.local/bin/
+cp rofi-wall-switcher $HOME/.local/bin/
+
+echo "Done! Log out and select Hyprland from your display manager or run start-hyprland if you're not using one."
+
+
+
+
